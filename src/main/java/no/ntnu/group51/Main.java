2f56e2f4fe2f916dbf9a8974c41c34ca57635e22
0xMathip <mathiasjhansen@outlook.com>
2026-02-23 17:01:52 +0100

    renamed folder structure

--- src/main/java/no/ntnu/group51/Main.java
+++ src/main/java/no/ntnu/group51/Main.java
@@ -1,4 +1,4 @@
-package no.ntnu.mathip;
+package no.ntnu.group51;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
