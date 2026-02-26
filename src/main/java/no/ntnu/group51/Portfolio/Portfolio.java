8f64fdbe3a4e8017c1a634307c9c657b4cf94bed
0xMathip <mathiasjhansen@outlook.com>
2026-02-26 23:25:59 +0100

    Fixed Share, Portfolio and exchange classes formatting with CheckStyle

--- src/main/java/no/ntnu/group51/Portfolio/Portfolio.java
+++ src/main/java/no/ntnu/group51/Portfolio/Portfolio.java
@@ -1,87 +1,90 @@
 package no.ntnu.group51.Portfolio;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Collections;
+import java.util.List;
 import no.ntnu.group51.Stocks.Share;
 
 /**
  * Portfolio class.
  */
 public class Portfolio {
-    private List<Share> shares;
+  private List<Share> shares;
 
-    /**
-     * Constructor for the Portfolio class.
-     * Creates a list with intention to add shares.
-     */
-    public Portfolio() {
-        this.shares = new ArrayList<>();
-    }
+  /**
+   * Constructor for the Portfolio class.
+   * Creates a list with intention to add shares.
+   */
+  public Portfolio() {
+    this.shares = new ArrayList<>();
+  }
 
-    /**
-     * Adds a share in the Portfolio-list.
-     *
-     * @param share the share you want to add.
-     * @return true if the share was added, false if it was not.
-     * @throws IllegalArgumentException if share is null.
-     */
-    public boolean addShare(Share share) {
-        if (share == null)
-            throw new IllegalArgumentException("Share cannot be null.");
-        return shares.add(share);
+  /**
+   * Adds a share in the Portfolio-list.
+   *
+   * @param share the share you want to add.
+   * @return true if the share was added, false if it was not.
+   * @throws IllegalArgumentException if share is null.
+   */
+  public boolean addShare(Share share) {
+    if (share == null) {
+      throw new IllegalArgumentException("Share cannot be null.");
     }
+    return shares.add(share);
+  }
 
-    /**
-     * Removes a share in the Portfolio-list.
-     *
-     * @param share the share you want to remove.
-     * @return true if the share was removed, false if it was not.
-     * @throws IllegalArgumentException if share is null.
-     */
-    public boolean removeShare (Share share) {
-        if (share == null)
-            throw new IllegalArgumentException("Share cannot be null.");
-        return shares.remove(share);
+  /**
+   * Removes a share in the Portfolio-list.
+   *
+   * @param share the share you want to remove.
+   * @return true if the share was removed, false if it was not.
+   * @throws IllegalArgumentException if share is null.
+   */
+  public boolean removeShare(Share share) {
+    if (share == null) {
+      throw new IllegalArgumentException("Share cannot be null.");
     }
+    return shares.remove(share);
+  }
 
-    /**
-     * Creates an unmodifiable list of the shares in the portfolio
-     *
-     * @return the unmodifiable list of the portfolio's shares.
-     */
-    public List<Share> getShares() {
-        return Collections.unmodifiableList(shares);
-    }
+  /**
+   * Creates an unmodifiable list of the shares in the portfolio.
+   *
+   * @return the unmodifiable list of the portfolio's shares.
+   */
+  public List<Share> getShares() {
+    return Collections.unmodifiableList(shares);
+  }
 
-    /**
-     * Returns the shares associated with a specific symbol.
-     * Searches through the portfolio list with the entered symbol,
-     * and adds it to a list.
-     *
-     * @param symbol the symbol for a stock, i.e "AAPL".
-     * @return a list of shares with the entered symbol.
-     * @throws IllegalArgumentException if symbol is null.
-     */
-    public List<Share> getShares(String symbol) {
-        if (symbol == null)
-            throw new IllegalArgumentException("Symbol cannot be null.");
-        List<Share> indexedList = shares.stream()
-                .filter(s -> s.getStock().getSymbol().equalsIgnoreCase(symbol))
-                .toList();
-        return indexedList;
+  /**
+   * Returns the shares associated with a specific symbol.
+   * Searches through the portfolio list with the entered symbol,
+   * and adds it to a list.
+   *
+   * @param symbol the symbol for a stock, i.e "AAPL".
+   * @return a list of shares with the entered symbol.
+   * @throws IllegalArgumentException if symbol is null.
+   */
+  public List<Share> getShares(String symbol) {
+    if (symbol == null) {
+      throw new IllegalArgumentException("Symbol cannot be null.");
     }
+    List<Share> indexedList =
+        shares.stream().filter(s -> s.getStock().getSymbol().equalsIgnoreCase(symbol)).toList();
+    return indexedList;
+  }
 
-    /**
-     * Checks if a share is in a portfolio
-     *
-     * @param share the share you want to check in a portfolio
-     * @return true if the portfolio contains the share, false if it does not.
-     * @throws IllegalArgumentException if symbol is null.
-     */
-    public boolean contains(Share share) {
-        if (share == null)
-            throw new IllegalArgumentException("Share cannot be null.");
-        return shares.contains(share);
+  /**
+   * Checks if a share is in a portfolio.
+   *
+   * @param share the share you want to check in a portfolio
+   * @return true if the portfolio contains the share, false if it does not.
+   * @throws IllegalArgumentException if symbol is null.
+   */
+  public boolean contains(Share share) {
+    if (share == null) {
+      throw new IllegalArgumentException("Share cannot be null.");
     }
+    return shares.contains(share);
+  }
 }
--- src/main/java/no/ntnu/group51/Stocks/Share.java
+++ src/main/java/no/ntnu/group51/Stocks/Share.java
@@ -3,60 +3,62 @@ package no.ntnu.group51.Stocks;
 import java.math.BigDecimal;
 
 /**
- * Share class
+ * Share class.
  */
 public class Share {
-    private final Stock stock;
-    private BigDecimal quantity;
-    private BigDecimal purchasePrice;
+  private final Stock stock;
+  private BigDecimal quantity;
+  private BigDecimal purchasePrice;
 
-    /**
-     * Constructor for the Share class.
-     *
-     * @throws IllegalArgumentException if stock is null,
-     *                                  if quantity is null, zero or negative,
-     *                                  if purchasePrice is null or negative.
-     *
-     * @param stock the purchased stock.
-     * @param quantity the amount of shares purchased.
-     * @param purchasePrice the purchase price of the share.
-     */
-    public Share(Stock stock, BigDecimal quantity, BigDecimal purchasePrice){
-        if (stock == null)
-            throw new IllegalArgumentException ("Stock cannot be null");
-        if (quantity == null || quantity.compareTo(BigDecimal.ZERO) <= 0)
-            throw new IllegalArgumentException("Quantity must be positive.");
-        if (purchasePrice == null || purchasePrice.compareTo(BigDecimal.ZERO) < 0)
-            throw new IllegalArgumentException("Purchase price cannot be negative.");
-        this.stock = stock;
-        this.quantity = quantity;
-        this.purchasePrice = purchasePrice;
+  /**
+   * Constructor for the Share class.
+   *
+   * @param stock         the purchased stock.
+   * @param quantity      the amount of shares purchased.
+   * @param purchasePrice the purchase price of the share.
+   * @throws IllegalArgumentException if stock is null,
+   *                                  if quantity is null, zero or negative,
+   *                                  if purchasePrice is null or negative.
+   */
+  public Share(Stock stock, BigDecimal quantity, BigDecimal purchasePrice) {
+    if (stock == null) {
+      throw new IllegalArgumentException("Stock cannot be null");
     }
+    if (quantity == null || quantity.compareTo(BigDecimal.ZERO) <= 0) {
+      throw new IllegalArgumentException("Quantity must be positive.");
+    }
+    if (purchasePrice == null || purchasePrice.compareTo(BigDecimal.ZERO) < 0) {
+      throw new IllegalArgumentException("Purchase price cannot be negative.");
+    }
+    this.stock = stock;
+    this.quantity = quantity;
+    this.purchasePrice = purchasePrice;
+  }
 
-    /**
-     * Returns the stock associated with a share object.
-     *
-     * @return the stock instance.
-     */
-    public Stock getStock() {
-        return stock;
-    }
+  /**
+   * Returns the stock associated with a share object.
+   *
+   * @return the stock instance.
+   */
+  public Stock getStock() {
+    return stock;
+  }
 
-    /**
-     * Returns the number of shares owned.
-     *
-     * @return the quantity of shares.
-     */
-    public BigDecimal getQuantity() {
-        return quantity;
-    }
+  /**
+   * Returns the number of shares owned.
+   *
+   * @return the quantity of shares.
+   */
+  public BigDecimal getQuantity() {
+    return quantity;
+  }
 
-    /**
-     * Returns the purchase price per share.
-     *
-     * @return the purchase price.
-     */
-    public BigDecimal getPurchasePrice() {
-        return purchasePrice;
-    }
+  /**
+   * Returns the purchase price per share.
+   *
+   * @return the purchase price.
+   */
+  public BigDecimal getPurchasePrice() {
+    return purchasePrice;
+  }
 }
